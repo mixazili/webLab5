adf0dc09f9352883a680f5e2ea9d00ab8eda8eb9
mixazili <138684944+mixazili@users.noreply.github.com>
2025-12-17 21:00:16 +0300

    Trigger CI/CD

--- tests/integration.test.js
+++ tests/integration.test.js
@@ -1,13 +1,19 @@
-import { describe, it, expect, beforeAll, afterAll } from "vitest";
-import request from "supertest";
 import mongoose from "mongoose";
+import { describe, beforeAll, afterAll, it, expect, vi } from "vitest";
+import request from "supertest";
 import app from "../src/app";
+
+vi.setTimeout(60000); // 60 секунд на все тесты
 
 const MONGO_URI = process.env.MONGO_URI;
 
 describe("Auth integration tests", () => {
   beforeAll(async () => {
-    await mongoose.connect(MONGO_URI);
+    await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 60000,
+    });
   });
 
   afterAll(async () => {
@@ -16,24 +22,18 @@ describe("Auth integration tests", () => {
   });
 
   it("should register user", async () => {
-    const res = await request(app)
-      .post("/api/auth/register")
-      .send({
-        username: "vitestuser",
-        password: "12345678",
-      });
-
+    const res = await request(app).post("/api/auth/register").send({
+      username: "vitestuser",
+      password: "12345678",
+    });
     expect(res.statusCode).toBe(201);
   });
 
   it("should login user and return access token", async () => {
-    const res = await request(app)
-      .post("/api/auth/login")
-      .send({
-        username: "vitestuser",
-        password: "12345678",
-      });
-
+    const res = await request(app).post("/api/auth/login").send({
+      username: "vitestuser",
+      password: "12345678",
+    });
     expect(res.statusCode).toBe(200);
     expect(res.body.accessToken).toBeDefined();
   });
